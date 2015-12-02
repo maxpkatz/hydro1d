1fbb8a46aa2e920b406c8325cc6e558f07eb2082
Max Katz <maximilian.katz@stonybrook.edu>
2015-12-02 11:40:21 -0500

    We were forgetting to adjust the kinetic energy to make sense for the co-moving frame -- we want to do this so that we get the correct internal energy.

--- godunov.f90
+++ godunov.f90
@@ -18,6 +18,8 @@ contains
     type(gridedgevar_t), intent(inout) :: U_l, U_r, vf
     real (kind=dp_t),    intent(in   ) :: dt
 
+    double precision :: rho_e_l, rho_e_r
+    
     integer :: i
 
     ! piecewise constant slopes
@@ -30,6 +32,15 @@ contains
        ! subtract off the face velocities
        U_l%data(i,2) = U_l%data(i,2) - U%data(i-1,1) * vf%data(i,1)
        U_r%data(i,2) = U_r%data(i,2) - U%data(i  ,1) * vf%data(i,1)
+
+       ! Subtract off corresponding kinetic energies. We do this by saving the
+       ! old (rho * e) and adding to it the kinetic energy in the co-moving frame.
+       
+       rho_e_l = U%data(i-1,3) - 0.5_dp_t * U%data(i-1,2)**2 / U%data(i-1,1)
+       rho_e_r = U%data(i  ,3) - 0.5_dp_t * U%data(i  ,2)**2 / U%data(i  ,1)
+       
+       U_l%data(i,3) = rho_e_l + 0.5_dp_t * U_l%data(i,2)**2 / U_l%data(i,1)
+       U_r%data(i,3) = rho_e_r + 0.5_dp_t * U_r%data(i,2)**2 / U_r%data(i,1)
     enddo
 
   end subroutine make_interface_states_godunov
