1e86019caa9dbaa2f25996d3ec4ce0d89fe6b75e
Max Katz <maximilian.katz@stonybrook.edu>
2015-12-03 14:43:26 -0500

    Fix the kinetic energy issue that was fixed in the riemann_version branch

--- godunov.f90
+++ godunov.f90
@@ -20,6 +20,8 @@ contains
 
     integer :: i
 
+    real (kind=dp_t) :: rho_e_l, rho_e_r
+
     ! piecewise constant slopes
 
     ! loop over interfaces and fill the states 
@@ -30,6 +32,16 @@ contains
        ! subtract off the face velocities
        U_l%data(i,2) = U_l%data(i,2) - U%data(i-1,1) * vf%data(i,1)
        U_r%data(i,2) = U_r%data(i,2) - U%data(i  ,1) * vf%data(i,1)
+
+
+       ! Subtract off corresponding kinetic energies. We do this by saving the
+       ! old (rho * e) and adding to it the kinetic energy in the co-moving frame.
+
+       rho_e_l = U%data(i-1,3) - 0.5_dp_t * U%data(i-1,2)**2 / U%data(i-1,1)
+       rho_e_r = U%data(i  ,3) - 0.5_dp_t * U%data(i  ,2)**2 / U%data(i  ,1)
+
+       U_l%data(i,3) = rho_e_l + 0.5_dp_t * U_l%data(i,2)**2 / U_l%data(i,1)
+       U_r%data(i,3) = rho_e_r + 0.5_dp_t * U_r%data(i,2)**2 / U_r%data(i,1)
     enddo
 
   end subroutine make_interface_states_godunov
