9b81f2a79e4a590a10068f57bccfad3c1dd09ab1
Max Katz <maximilian.katz@stonybrook.edu>
2015-12-06 16:35:59 -0500

    Add some fractions

--- datatypes.f90
+++ datatypes.f90
@@ -9,6 +9,11 @@ module datatypes_module
   real (kind=dp_t), parameter :: ONE   = 1.0_dp_t
   real (kind=dp_t), parameter :: TWO   = 2.0_dp_t
   real (kind=dp_t), parameter :: THREE = 3.0_dp_t
+  real (kind=dp_t), parameter :: FOUR  = 4.0_dp_t
   real (kind=dp_t), parameter :: SIX   = 6.0_dp_t
+
+  real (kind=dp_t), parameter :: FOURTH = ONE / FOUR
+  real (kind=dp_t), parameter :: TWO3RD = TWO / THREE
+  real (kind=dp_t), parameter :: SIXTH  = ONE / SIX
   
 end module datatypes_module
